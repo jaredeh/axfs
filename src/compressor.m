d8241ef31de7954d077333ff51e67b3a9fb43505
Jared Hulbert <jaredeh@gmail.com>
2012-11-23 01:14:33 -0800

    compression quiet

--- src/compressor.m
+++ src/compressor.m
@@ -31,11 +31,12 @@
 	fwrite(data,size,1,fp);
 	fclose(fp);
 */
-
+/*
 	printf("\n1 csize: 0x%08llx\n",(unsigned long long)*csize);
 	printf("1 size: 0x%08llx\t%i\n",(unsigned long long)size,(int)size);
 	printf("1 data: \t%i\t0x%08llx\t%i\n",(int)data,(unsigned long long)data,(int)data);
 	printf("1 cdata: \t%i\t0x%08llx\n",(int)cdata,(unsigned long long)cdata);
+*/
 
 	if (size == 0)
 		return;
@@ -49,10 +50,12 @@
 
 	*csize = compress->compress(stream, cdata, data, size, size, &error);
 
+/*
 	printf("\n2 csize: 0x%08llx\n",(unsigned long long)*csize);
 	printf("2 size: 0x%08llx\n",(unsigned long long)size);
 	printf("2 data: \t%i\t0x%08llx\n",(int)data,(unsigned long long)data);
 	printf("2 cdata: \t%i\t0x%08llx\n",(int)cdata,(unsigned long long)cdata);
+*/
 /*
 	fp = fopen("data.bin", "w+");
 	fwrite(data,size,1,fp);
