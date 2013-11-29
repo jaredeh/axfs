d297ea58fdba7d14358da6d8c7049e731ef1160e
jared <Jared Hulbert>
2013-11-29 01:36:06 -0800

    mkdir linux

--- kernel/build/make_kernel.rb
+++ kernel/build/make_kernel.rb
@@ -1,34 +1,3 @@
-kernels = [
-  "v2.6.25",
-  "v2.6.26",
-  "v2.6.27",
-  "v2.6.28",
-  "v2.6.29",
-  "v2.6.30",
-  "v2.6.31",
-  "v2.6.32",
-  "v2.6.33",
-  "v2.6.34",
-  "v2.6.35",
-  "v2.6.36",
-  "v2.6.37",
-  "v2.6.38",
-  "v2.6.39",
-  "v3.0",
-  "v3.1",
-  "v3.2",
-  "v3.3",
-  "v3.4",
-  "v3.5",
-  "v3.6",
-  "v3.7",
-  "v3.8",
-  "v3.9",
-  "v3.10",
-  "v3.11",
-  "v3.12"
-]
-
 def run(cmd)
   puts cmd
   pid, stdin, stdout, stderr = Open4::popen4 "bash"
@@ -45,10 +14,10 @@ def run(cmd)
 end
 
 def build(options)
-  k = options[:kernel]
   if not File.exists?(options[:kernel])
     run "git clone --no-checkout --reference /opt/linux_git https://github.com/torvalds/linux.git #{options[:kernel]}"
   end
+  startdir = Dir.pwd
   Dir.chdir options[:kernel]
   run "git checkout -f #{options[:kernel]}"
   run "make mrproper"
@@ -68,6 +37,34 @@ def build(options)
   if options[:build]
     run "make -j 9; make"
   end
+  Dir.chdir startdir
+end
+
+def build(options)
+  if not File.exists?(options[:kernel])
+    run "git clone --no-checkout --reference /opt/linux_git https://github.com/torvalds/linux.git #{options[:kernel]}"
+  end
+  startdir = Dir.pwd
+  Dir.chdir options[:kernel]
+  run "git checkout -f #{options[:kernel]}"
+  run "make mrproper"
+  if options[:patch]
+    run "perl ../../tools/patchin.pl --assume-yes --link"
+  end
+  if options[:config]
+    run "make defconfig"
+    run "echo \"CONFIG_AXFS=#{options[:config]}\" >> .config"
+    if options[:profiling]
+      run "echo \"CONFIG_AXFS_PROFILING=#{options[:profiling]}\" >> .config"
+    end
+    run "make silentoldconfig"
+  elsif options[:build]
+    run "make defconfig"
+  end
+  if options[:build]
+    run "make -j 9; make"
+  end
+  Dir.chdir startdir
 end
 
 require 'optparse'
@@ -102,5 +99,7 @@ end
 
 Dir.chdir File.join(File.dirname(__FILE__),"linux")
 puts options
+`mkdir -p linux`
 puts Dir.pwd
 build(options)
+test(options)
