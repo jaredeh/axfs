8160106003708d195a7978d5492b2314b8e112b8
jared <Jared Hulbert>
2013-12-17 23:42:01 -0800

    mount get_sb

--- kernel/fs/axfs/axfs_super.c
+++ kernel/fs/axfs/axfs_super.c
@@ -1074,8 +1074,8 @@ static struct super_operations axfs_sops = {
 static struct file_system_type axfs_fs_type = {
 	.owner = THIS_MODULE,
 	.name = "axfs",
-#if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,38)
-	.mount = axfs_get_sb,
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,36)
+	.mount = axfs_mount,
 #else
 	.get_sb = axfs_get_sb,
 #endif
