6b280e2d0e38bb4db2774c0e63ddfd1c367b916b
jared <Jared Hulbert>
2014-02-05 03:03:35 -0800

    run_emu scripts

--- tests/qemu/install/make_hda.sh
+++ tests/qemu/install/make_hda.sh
@@ -3,8 +3,11 @@ for ARCH in arm mips ppc x86 x86-64
 do
   mkdir -p $ARCH
   cd $ARCH
-  wget http://downloads.yoctoproject.org/releases/yocto/yocto-1.4.2/machines/qemu/qemu$ARCH/core-image-lsb-sdk-qemu$ARCH.tar.bz2
-  #cp /home/jared/projects/axfs/qemu/yocto/$ARCH/core-image-lsb-sdk-qemu$ARCH.tar.bz2 .
+  if ! [ -f core-image-lsb-sdk-qemu$ARCH.tar.bz2 ]
+    then
+      wget http://downloads.yoctoproject.org/releases/yocto/yocto-1.4.2/machines/qemu/qemu$ARCH/core-image-lsb-sdk-qemu$ARCH.tar.bz2
+      #cp /home/jared/projects/axfs/qemu/yocto/$ARCH/core-image-lsb-sdk-qemu$ARCH.tar.bz2 .
+  fi
   mkdir -p mnt
   sudo rm -rf hda.ext2
   sudo fallocate -l 5000000000 hda.ext2
